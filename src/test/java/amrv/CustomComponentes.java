f67974ca8ac8670a1dcc02f421df48f772b3195c
Ansuz <adrianmrv02@gmail.com>
2022-05-30 02:09:49 +0200

    Created component tests

--- src/test/java/amrv/CustomComponentes.java
+++ src/test/java/amrv/CustomComponentes.java
@@ -1,12 +1,9 @@
 package amrv;
 
 import com.formdev.flatlaf.FlatDarkLaf;
-import java.awt.BorderLayout;
-import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.WindowConstants;
-import org.amrvimag.bocateria.swing.ASearchBox;
+import org.amrvimag.bocateria.view.ViewWrapper;
 
 /**
  *
@@ -16,19 +13,9 @@ public class CustomComponentes {
  
     public static void main(String[] args) throws UnsupportedLookAndFeelException {
         FlatDarkLaf.setup();
-        UIManager.setLookAndFeel(new FlatDarkLaf());
+        ViewWrapper.setLookAndFeel("");
         
-        JFrame base = new JFrame("Prueba");
-        ASearchBox box = new ASearchBox();
-        box.addItem("Hola");
-        box.addItem("Que tal");
-        box.setEditable(true);
-        
-        base.setSize(500,500);
-        base.add(box,BorderLayout.NORTH);
-        base.setLocationRelativeTo(null);
-        base.setVisible(true);
-        base.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        UIManager.set
     }
     
 }
