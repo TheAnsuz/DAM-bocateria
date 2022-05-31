2e22d540c0bc910b86679637e3c0b9791c3df9a9
AMRV <adrianmrv02@gmail.com>
2022-05-31 13:55:34 +0200

    Second anttempt to create a table of buttons

---
+++ src/main/java/org/amrvimag/bocateria/view/CellRenderer.java
@@ -0,0 +1,18 @@
+package org.amrvimag.bocateria.view;
+
+import java.awt.Component;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+/**
+ *
+ * @author Adrian MRV. aka AMRV || Ansuz (org.amrv)
+ */
+public class CellRenderer implements TableCellRenderer {
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        return null;
+    }
+
+}
