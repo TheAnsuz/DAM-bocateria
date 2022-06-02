761fff17587019f59f256b2477269aca948cfe18
Ansuz <adrianmrv02@gmail.com>
2022-06-03 01:55:54 +0200

    Implemented Configuration management to database connection

--- src/main/java/org/amrvimag/bocateria/model/resources/ConnectionDB.java
+++ src/main/java/org/amrvimag/bocateria/model/resources/ConnectionDB.java
@@ -3,20 +3,38 @@ package org.amrvimag.bocateria.model.resources;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import org.amrvimag.bocateria.Configuration;
+import org.amrvimag.bocateria.view.ViewWrapper;
 
-public class ConnectionDB {
+public class ConnectionDB implements Configuration.ConfigurationListener {
 
     private static Connection con;
 
     public static void createConnection(String url, String usr, String password) {
-        if (con != null) return;
+        if (con != null)
+            return;
 
         try {
             con = DriverManager.getConnection(url, usr, password);
-        } catch (SQLException e) { e.printStackTrace(); }
+        } catch (SQLException e) {
+            ViewWrapper.getView().showError(e);
+        }
     }
 
     public static Connection getConnection() {
         return con;
+    }
+
+    @Override
+    public void onChange(String key, String oldValue, String newValue) {
+        switch (key) {
+            case "sql.url":
+            case "sql.username":
+            case "sql.password":
+                con = null;
+                createConnection(Configuration.getConfig("sql.url"), Configuration
+                        .getConfig("sql.username"), Configuration
+                        .getConfig("sql.password"));
+        }
     }
 }
